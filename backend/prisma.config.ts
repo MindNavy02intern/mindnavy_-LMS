bd3438e0c7d6ac4b8a03be15e99b8e22dc2d9d57
mn3intern <mn03intern@mindnavy.com>
2026-05-22 11:49:52 +0300

    update backend

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from '@prisma/config'
 import 'dotenv/config'
 
 export default defineConfig({
-  schema: './prisma/schema.prisma',
+  schema: './prisma/schema/schema.prisma',
 
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DIRECT_URL!,
   },
 })
